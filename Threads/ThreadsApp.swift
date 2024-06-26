dd2d5fe83b695bcf73dd3f62c3c2809f4139de49
HarmonicVoid <marvin.bolanos25@gmail.com>
2024-06-26 15:20:36 -0700

    Initial Commit

---
+++ Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by Marvin Bolanos on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
