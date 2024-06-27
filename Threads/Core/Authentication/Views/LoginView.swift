7470b983a0c185dfb2057010aeb751d48eff40c1
HarmonicVoid <marvin.bolanos25@gmail.com>
2024-06-26 18:16:45 -0700

    Add user login page

---
+++ Threads/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Threads
+//
+//  Created by Marvin Bolanos on 6/26/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
