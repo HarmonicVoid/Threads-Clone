2ddb70d8929cd83c538ea3b912f09ae1bf6f9b87
HarmonicVoid <marvin.bolanos25@gmail.com>
2024-07-01 09:02:43 -0700

    Feature: Create Explore View

--- Threads/Core/Explore/View/ExploreView.swift
+++ Threads/Core/Explore/View/ExploreView.swift
@@ -8,8 +8,55 @@
 import SwiftUI
 
 struct ExploreView: View {
+    @State private var searchText = ""
+    
     var body: some View {
-        Text("Hello, Explore!")
+        NavigationStack {
+            ScrollView {
+                LazyVStack {
+                    ForEach(0 ... 10, id: \.self) { user in
+                        VStack {
+                            HStack {
+                                Image("Max_Verstappen")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 40, height: 30)
+                                    .clipShape(Circle())
+                                
+                                VStack {
+                                    Text("maxverstappen")
+                                        .fontWeight(.semibold)
+                                    
+                                    Text("Max Verstappen")
+        
+                                }
+                                .font(.footnote)
+                                
+                                Spacer()
+                                
+                                Text("Follow")
+                                    .font(.subheadline)
+                                    .fontWeight(.semibold)
+                                    .frame(width: 100, height: 32)
+                                    .overlay {
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(Color(.systemGray4))
+                                    }
+
+                            }
+                            .padding(.horizontal)
+                            
+                            Divider()
+                        }
+                        .padding(.vertical, 4)
+                        
+                    }
+                    
+                }
+            }
+            .navigationTitle("Serach")
+            .searchable(text:$searchText , prompt: "Search" )
+        }
     }
 }
 
