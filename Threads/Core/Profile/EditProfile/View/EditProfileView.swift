a126e9e0b31ef157c60dee41f4b4adaab5f0d433
HarmonicVoid <marvin.bolanos25@gmail.com>
2024-07-07 09:07:22 -0700

    Create Edit Profile View

---
+++ Threads/Core/Profile/EditProfile/View/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  Threads
+//
+//  Created by Marvin Bolanos on 7/7/24.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
